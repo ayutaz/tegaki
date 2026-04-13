bfa32c9c9a7c959a522461a98dfe4c54aedd24a4
Gokhan Kurt <krtgkn@gmail.com>
2026-04-13 14:12:18 +0300

    docs: add promo animation

--- examples/remotion/src/Root.tsx
+++ examples/remotion/src/Root.tsx
@@ -1,6 +1,7 @@
 import { Composition, useCurrentFrame, useVideoConfig } from 'remotion';
 import { TegakiRenderer } from 'tegaki';
 import caveat from 'tegaki/fonts/caveat';
+import { PROMO_DURATION, PROMO_FPS, PROMO_HEIGHT, PROMO_WIDTH, Promo } from './Promo';
 
 const Handwriting = () => {
   const frame = useCurrentFrame();
@@ -19,5 +20,17 @@ const Handwriting = () => {
 };
 
 export const RemotionRoot: React.FC = () => {
-  return <Composition id="Handwriting" component={Handwriting} durationInFrames={180} fps={60} width={1920} height={1080} />;
+  return (
+    <>
+      <Composition id="Handwriting" component={Handwriting} durationInFrames={180} fps={60} width={1920} height={1080} />
+      <Composition
+        id="Promo"
+        component={Promo}
+        durationInFrames={PROMO_DURATION}
+        fps={PROMO_FPS}
+        width={PROMO_WIDTH}
+        height={PROMO_HEIGHT}
+      />
+    </>
+  );
 };
